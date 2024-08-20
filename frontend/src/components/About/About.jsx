4925c8f1aa7f123314ca9810d25e9bcdc4d3b25a
ThiPhucThinh <thiphucthinhhuynh@gmail.com>
2024-08-19 22:57:25 -0700

    Add the Projects component

--- frontend/src/components/About/About.jsx
+++ frontend/src/components/About/About.jsx
@@ -5,6 +5,7 @@ const About = () => {
         <div className="about-section">
             <p>ABOUT</p>
             <p>Thi Phuc Thinh Huynh</p>
+            <p>Thi Huynh is a full stack web developer with a background in computer science, recently graduating from App Academy’s Full Stack Web Development BootCamp. Combining a passion for tech with a problem-solving mindset, Thi creates efficient, innovative web applications using JavaScript, Python, React, Flask, and more. A collaborative team player who thrives on continuous learning, Thi is dedicated to bringing creative solutions and exceptional value to future projects.</p>
         </div>
     );
 };
--- frontend/src/components/Projects/Projects.jsx
+++ frontend/src/components/Projects/Projects.jsx
@@ -1,9 +1,34 @@
 import './Projects.css';
+import { BsGithub } from "react-icons/bs";
+import { BsBoxArrowUpRight } from "react-icons/bs";
 
 const Projects = () => {
     return (
         <div className="project-section">
             <p>PROJECTS</p>
+
+            <div className="project-container">
+                <div id="nooknet">
+                    <img src="https://i.imghippo.com/files/VN9wP1724130745.png" alt="Nooknet" />
+                    <p>Nooknet</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    <div className="button-container">
+                        <button><BsGithub /> CODE</button>
+                        <button><BsBoxArrowUpRight /> LIVE</button>
+                    </div>
+                </div>
+
+                <div id="tumblr">
+                    <img src="https://i.imghippo.com/files/lEl0a1724130751.png" alt="Tumblr-Project" />
+                    <p>Tumblr-Project</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    <div className="button-container">
+                        <button><BsGithub /> CODE</button>
+                        <button><BsBoxArrowUpRight /> LIVE</button>
+                    </div>
+                </div>
+            </div>
+
         </div>
     );
 };
