b3361e5cb3edb2661bbadd7eb718704ee86f7c67
ThiPhucThinh <thiphucthinhhuynh@gmail.com>
2024-08-19 23:59:40 -0700

    Style the Project children

--- frontend/src/components/Projects/Projects.jsx
+++ frontend/src/components/Projects/Projects.jsx
@@ -9,9 +9,9 @@ const Projects = () => {
 
             <div className="project-container">
                 <div id="nooknet">
-                    <img src="https://i.imghippo.com/files/VN9wP1724130745.png" alt="Nooknet" />
-                    <p>Nooknet</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    <img src="https://i.imghippo.com/files/zNdTO1724134424.png" alt="Nooknet" />
+                    <p className="name">Nooknet</p>
+                    <p className="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                     <div className="button-container">
                         <button><BsGithub /> CODE</button>
                         <button><BsBoxArrowUpRight /> LIVE</button>
@@ -19,9 +19,9 @@ const Projects = () => {
                 </div>
 
                 <div id="tumblr">
-                    <img src="https://i.imghippo.com/files/lEl0a1724130751.png" alt="Tumblr-Project" />
-                    <p>Tumblr-Project</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    <img src="https://i.imghippo.com/files/9DMVW1724134432.png" alt="Tumblr-Project" />
+                    <p className="name">Tumblr-Project</p>
+                    <p className="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                     <div className="button-container">
                         <button><BsGithub /> CODE</button>
                         <button><BsBoxArrowUpRight /> LIVE</button>
